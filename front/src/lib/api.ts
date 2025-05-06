ba206a768808bb9576ade3190ecbf51c48d1ceb1
yosr-s <115195080+yosr-s@users.noreply.github.com>
2025-05-06 03:36:01 +0200

    Update api.ts

--- front/src/lib/api.ts
+++ front/src/lib/api.ts
@@ -4,7 +4,8 @@ import axios from 'axios';
 const api = axios.create({
   //baseURL: 'http://localhost:5000/api', 
   //baseURL: 'http://172.20.10.2:5000/api', 
-    baseURL: 'https://arabian-nights-backend-1t2e.onrender.com/api', 
+   // baseURL: 'https://arabian-nights-backend-1t2e.onrender.com/api', 
+  baseURL: 'arabian-nights.vercel.app',
 
 
 });
