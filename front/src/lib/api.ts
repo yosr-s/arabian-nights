0e7615e95845a0516c8a54010bffc90eaa5e552d
yosr-s <115195080+yosr-s@users.noreply.github.com>
2025-05-06 20:07:54 +0200

    Update api.ts

--- front/src/lib/api.ts
+++ front/src/lib/api.ts
@@ -1,12 +1,12 @@
 // src/lib/api.ts
 import axios from 'axios';
 
-axios.defaults.withCredentials = true;
+//axios.defaults.withCredentials = true;
 const api = axios.create({
   //baseURL: 'http://localhost:5000/api', 
   //baseURL: 'http://172.20.10.2:5000/api', 
-   // baseURL: 'https://arabian-nights-backend-1t2e.onrender.com/api', 
-  baseURL: 'https://arabian-nights.vercel.app/api',
+    baseURL: 'https://arabian-nights-backend-1t2e.onrender.com/api', 
+  //baseURL: 'https://arabian-nights.vercel.app/api',
 
 
 });
