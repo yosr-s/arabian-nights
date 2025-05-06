558c54a66bdb62f6c37e195caec2ac8717196936
yosr-s <115195080+yosr-s@users.noreply.github.com>
2025-05-06 03:39:19 +0200

    Update api.ts

--- front/src/lib/api.ts
+++ front/src/lib/api.ts
@@ -1,6 +1,7 @@
 // src/lib/api.ts
 import axios from 'axios';
 
+axios.defaults.withCredentials = true;
 const api = axios.create({
   //baseURL: 'http://localhost:5000/api', 
   //baseURL: 'http://172.20.10.2:5000/api', 
